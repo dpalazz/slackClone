1d1af3354df27afc1e6f1d26b3cbd8c1781935c0
Dana Palazzo <dana@fretless.com>
2018-12-21 10:54:17 -0500

    Display fake data for current room and add input UI for messages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public rooms = ['room1', 'room2', 'room3'];
+  public currentRoom: string;
 
+  ngOnInit() {
+    this.currentRoom = this.rooms[0];
+  }
 }

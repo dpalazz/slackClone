6d2b34aab57652990470e6388a886cbe7560256f
Dana Palazzo <dana@fretless.com>
2019-01-18 10:33:06 -0500

    Display messages in current room

--- src/app/room.ts
+++ src/app/room.ts
@@ -2,12 +2,12 @@ export const rooms = [
   {
     id: 1,
     name: 'bookclub',
-    messages: [{user: '', message: 'Hey'}, {user: '', message: `Hey, what's going on? Did you finish your new book?`}]
+    messages: [{user: 'thenke', message: 'Hey'}, {user: 'dpalazzo', message: `Hey, what's going on? Did you finish your new book?`}]
   },
   {
     id: 2,
     name: 'movies',
-    messages: [{user: '', message: 'Yoo, did you see that new flick?!'}, {user: '', message: `Naw, man, I didn't.`}]
+    messages: [{user: 'thenke', message: 'Yoo, did you see that new flick?!'}, {user: 'dpalazzo', message: `Naw, man, I didn't.`}]
   }
 ];
 

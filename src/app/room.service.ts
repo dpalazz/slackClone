1aec5d9dc225fc8224541091e55a9b3ea6cb4a57
Dana Palazzo <dana@fretless.com>
2019-01-18 10:00:37 -0500

    Add RoomService

---
+++ src/app/room.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { rooms } from './room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor() { }
+
+  getRooms() {
+    return rooms;
+  }
+
+}

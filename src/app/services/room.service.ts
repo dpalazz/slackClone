67ac0915680d59f415e8fb089744348295d6022f
Dana Palazzo <dana@fretless.com>
2019-01-22 10:05:41 -0500

    Add message to currentRoom

--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 
 import { rooms } from '../room';
+import { Room } from '../models/room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
+  public rooms = rooms;
 
   constructor() { }
 
   getRooms() {
     return rooms;
+  }
+
+  addMessage(currentRoom: Room, message: any) {
+    this.rooms.map(room => {
+      if (currentRoom.name === room.name) {
+        currentRoom.messages.push(message);
+      }
+    });
   }
 
 }
